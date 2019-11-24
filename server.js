8ec8b9ce970ffca40367d0aae7683c0cb087d955
Sal Tamay <sal.tamay@outlook.com>
2019-11-24 17:08:11 -0500

    Add delete route to be able to delete notes

--- server.js
+++ server.js
@@ -47,19 +47,44 @@ app.post('/api/notes', (req, res, next) => {
                 }
 
                 data = JSON.parse(data);
+                // Set new notes id
+                newNote.id = data.length;
                 data.push(newNote);
-                console.log(JSON.stringify(data));
 
                 fs.writeFile(__dirname + '/db/db.json', JSON.stringify(data), err => {
                     if (err) throw err;
                     console.log('Success.')
                 });
-            })
-            res.status(201).send(newNote);
+            });
+            res.send(newNote);
         } else {
             throw new Error('Something went wrong.');
         }
     });
+})
+
+app.delete('/api/notes/:id', (req, res, next) => {
+    // Get the id of the note being deleted
+    const id = req.params.id;
+
+    fs.readFile(__dirname + '/db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        data = JSON.parse(data);
+        // Note's id corresponds the index position in notesArray
+        data.splice(id, 1);
+
+        // Rewrite the updated notes array
+        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(data), err => {
+            if (err) throw err;
+
+            console.log('Success.')
+        });
+    });
+
+    res.send('Deleted.');
 })
 
 app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`))
