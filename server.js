eeca86fa4c913b39417fd89d10fa36a1dc98fc53
Sal Tamay <sal.tamay@outlook.com>
2019-11-24 16:03:37 -0500

    Add api/notes route to render saved notes

--- server.js
+++ server.js
@@ -15,4 +15,21 @@ app.get('/notes', (req, res, next) => {
     res.status(200).sendFile(__dirname + '/public/notes.html');
 });
 
+app.get('/api/notes', (req, res, next) => {
+    try {
+        fs.readFile(__dirname + '/db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                throw Error(err);
+            }
+
+            const jsonData = JSON.parse(data);
+            res.status(200).send(jsonData);
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(404).send();
+    }
+
+})
+
 app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`))
